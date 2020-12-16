e0e5188a08681db49af40829ad83b83837ead7c7
Bailey Dunning <bwdunning@gmail.com>
2020-12-16 09:26:08 -0700

    Set up API call to get reservation data

--- src/App/App.js
+++ src/App/App.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react'
 import Form from '../Form/Form'
 import Container from '../Container/Container'
 import Card from '../Card/Card'
+import { getReservationData } from '../apiCalls'
 import './App.css'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-
+      reservations: [],
+      error: ''
     }
+  }
+
+  componentDidMount() {
+    getReservationData()
+    .then(data => this.setState({reservations: data}))
+    .catch(error => this.setState({error: error.message}))
   }
 
   render() {
